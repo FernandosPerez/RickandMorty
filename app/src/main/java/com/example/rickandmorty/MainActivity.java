58d7443d7e55e605605dfcdb6719ff014174e76e
FernandosPerez <82854646+FernandosPerez@users.noreply.github.com>
2022-06-22 23:22:19 -0500

    creación de la app

---
+++ app/src/main/java/com/example/rickandmorty/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.rickandmorty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView2,textView3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                startActivity(new Intent(MainActivity.this,activityrec.class));
+
+            }
+        });
+
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                startActivity(new Intent(MainActivity.this,registrar.class));
+
+            }
+        });
+
+
+    }
+}
