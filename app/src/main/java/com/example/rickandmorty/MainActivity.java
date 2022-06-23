ff51a5f74bc3a21777e297628e0fc0b9e8dc990f
FernandosPerez <82854646+FernandosPerez@users.noreply.github.com>
2022-06-23 01:20:29 -0500

    integracion del main draw

--- app/src/main/java/com/example/rickandmorty/MainActivity.java
+++ app/src/main/java/com/example/rickandmorty/MainActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView2,textView3;
-
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
 
                 startActivity(new Intent(MainActivity.this,activityrec.class));
+
+            }
+        });
+
+        button2= (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
 
             }
         });
